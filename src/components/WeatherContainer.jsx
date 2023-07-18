fd9f6cc2f59d1cdd09050be75e26790657e1448d
Joaquim Chianca <joaquimchianca@gmail.com>
2023-07-18 03:47:47 -0300

    feat: previsao do tempo

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 // import axios from 'axios'
+import './forecast.css'
 // import Forecast from './Forecast'
 
-function WeatherContainer({ data }) {
+function WeatherContainer({ data, forecast }) {
+
+  function format(dateInput) {
+    const [datePart, timePart] = dateInput.split(' ')
+    const [year, month, day] = datePart.split('-')
+    return `${day}/${month}`
+  }
 
   return (
     <div className="container">
@@ -21,14 +28,24 @@ function WeatherContainer({ data }) {
         {/* </div> */}
       </div>
       <div className="center">
-        <div className="forecast">
+        {/* <div className="forecast"> */}
           <div className="forecast-item">
-            <p>Amanhã</p>
-            <p>{}</p>
+            <p className='bold'>{format(forecast[8].dt_txt)}</p>
+            <p>{forecast ? `${forecast[8].main.temp} °C` : null}</p>
           </div>
-          <div className="forecast-item"></div>
-          <div className="forecast-item"></div>
-        </div>
+          <div className="forecast-item">
+            <p className='bold'>{format(forecast[16].dt_txt)}</p>
+            <p>{forecast ? `${forecast[16].main.temp} °C` : null}</p>
+          </div>
+          <div className="forecast-item">
+            <p className='bold'>{format(forecast[24].dt_txt)}</p>
+            <p>{forecast ? `${forecast[24].main.temp} °C` : null}</p>
+          </div>
+          <div className="forecast-item">
+            <p className='bold'>{format(forecast[32].dt_txt)}</p>
+            <p>{forecast ? `${forecast[32].main.temp} °C` : null}</p>
+          </div>
+        {/* </div> */}
       </div>
       <div className="bottom">
         {/* <div className="temperature">
