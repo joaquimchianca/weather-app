1115dc7b2975224a19900c59eb4ab9b8c3e527b8
Joaquim Chianca <joaquimchianca@gmail.com>
2023-07-18 03:02:21 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import WeatherContainer from './components/WeatherContainer'
+
+function App() {
+
+	const [location, setLocation] = useState('')  //string de pesquisa passada como parametro para request da API
+	const [data, setData] = useState({})          //objeto que guarda resposta da API
+	const [forecast, setForecast] = useState({})
+	// estado que controla exibição das informações
+	const [showElements, setShowElements] = useState(false)
+	// define imagem de background dinamica
+	const [backgroundImage, setBackgroundImage] = useState('sunset')
+
+
+	const url_weather = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=fa4c2c684f89015fca9e5370e5cf2547&units=metric&lang=pt_br`
+	const url_forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=fa4c2c684f89015fca9e5370e5cf2547&units=metric&lang=pt_br`
+	// chamada da api com axios para buscar previsao atual
+	const searchLocation = (event) => {
+		if (event.key === 'Enter') {
+			axios
+				.get(url_weather).then(response => {
+					setData(response.data)
+					setShowElements(true)
+					//console.log(response.data)
+				})
+				.catch(error => {
+					if (error.response && error.response.status === 404) {
+						alert('Localização não encontrada')
+					} else {
+						alert('Erro na requisição')
+					}
+				})
+		}
+	}
+	
+	// chamada da api de previsao de tempo
+	const searchForecast = (event) => {
+		if (event.key === "Enter") {
+			axios.get(url_forecast).then(response => {
+				setForecast(response.list)
+			})
+		}
+	}
+
+	function search(event) {
+		searchLocation(event);
+		searchForecast(event);
+	}
+
+	// atualizacao da imagem de fundo
+	useEffect(() => {
+		if (data.weather) {
+			setBackgroundImage(getBackgroundImage(data.weather[0].id))
+		}
+	}, [data])
+
+	// define qual a imagem dependendo do clima
+	const getBackgroundImage = (id) => {
+		if (id >= 200 && id < 300) {
+			return 'thunderstorm'
+		} else if (id >= 300 && id < 400) {
+			return 'drizzle'
+		} else if (id >= 500 && id < 600) {
+			return 'rain'
+		} else if (id >= 600 && id < 700) {
+			return 'snow'
+		} else if (id >= 700 && id < 800) {
+			return 'mist'
+		} else if (id === 800) {
+			return 'clear'
+		} else if (id > 800) {
+			return 'clouds'
+		} else {
+			return 'sunset'
+		}
+	}
+
+
+
+	return (
+		<div className={`app ${backgroundImage}`}>
+			<div className="search">
+				<input
+					value={location}
+					type="text"
+					placeholder="Enter Location"
+					onChange={event => setLocation(event.target.value)}
+					onKeyDown={event => searchLocation(event)} />
+			</div>
+
+			{showElements && (<WeatherContainer data={data} />)}
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/WeatherContainer.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+// import axios from 'axios'
+// import Forecast from './Forecast'
+
+function WeatherContainer({ data }) {
+
+  return (
+    <div className="container">
+      <div className="top">
+        {/* <div className="current"> */}
+          <div className="location">
+            <p>{data.name}{data.sys ? `, ${data.sys.country}` : null}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp} °C</h1> : null}
+          </div>
+          <div className="description">
+            <p>{data.weather ? data.weather[0].description : null}</p>
+          </div>
+          {/* <Forecast data={data}/> */}
+        {/* </div> */}
+      </div>
+      <div className="center">
+        <div className="forecast">
+          <div className="forecast-item">
+            <p>Amanhã</p>
+            <p>{}</p>
+          </div>
+          <div className="forecast-item"></div>
+          <div className="forecast-item"></div>
+        </div>
+      </div>
+      <div className="bottom">
+        {/* <div className="temperature">
+          <div className="max">
+            <p>Máx: </p>
+            <p>{data.main ? `${data.main.temp_max} °C` : null}</p>
+          </div>
+          <div className="min">
+            <p>Mín:</p>
+            <p>{data.main ? `${data.main.temp_min} °C` : null}</p>
+          </div>
+        </div> */}
+        <div className="other-info">
+          <div className="feels">
+            <p className='bold'>{data.main ? `${data.main.feels_like} °C` : null}</p>
+            <p className='caption'>Sensação térmica</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>{data.main ? `${data.main.humidity}%` : null}</p>
+            <p className='caption'>Umidade</p>
+          </div>
+          <div className="wind">
+            <p className='bold'>{data.wind ? `${data.wind.speed} m/s` : null}</p>
+            <p className='caption'>Velocidade do vento</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WeatherContainer
